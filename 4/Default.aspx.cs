067e7589d46f5ddc9d82091f9bff72795669121d
Sandip Sonar <sandipsonar119512@gmail.com>
2024-10-15 12:34:06 +0530

    Initial commit

---
+++ 4/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _4_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+    {
+        String str = args.Value;
+        
+        if (str.Length>3)
+        {
+            args.IsValid = true;
+        }
+        else
+        {
+            args.IsValid = false;
+        }
+    }
+}
